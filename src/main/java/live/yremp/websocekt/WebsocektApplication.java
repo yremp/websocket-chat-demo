6bb8cb0803bb34af55f6301bfaeaa3ead929ec7f
yremp <737784215@qq.com>
2019-09-21 13:12:28 +0800

    spring boot websocket 在线聊天

---
+++ src/main/java/live/yremp/websocekt/WebsocektApplication.java
@@ -0,0 +1,13 @@
+package live.yremp.websocekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocektApplication.class, args);
+    }
+
+}
---
+++ src/main/java/live/yremp/websocekt/entitry/ChatMessage.java
@@ -0,0 +1,16 @@
+package live.yremp.websocekt.entitry;
+
+import lombok.Data;
+
+@Data
+public class ChatMessage {
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+}
